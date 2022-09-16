f20d8b4e06891c7c604f68c08a3a21caf10677cd
jialan <jialan@dtstack.com>
2022-09-16 23:24:06 +0800

    init project

---
+++ src/pages/Content/index.js
@@ -0,0 +1,4 @@
+
+console.log('Content script works!');
+console.log('Must reload extension for modifications to take effect.');
+
---
+++ src/pages/Options/Options.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Options.css';
+import {Input} from 'antd'
+import 'antd/dist/antd.css'
+interface Props {
+  title: string;
+}
+
+const Options: React.FC<Props> = ({ title }: Props) => {
+  return <div className="OptionsContainer">{title} 
+    <Input />
+  </div>;
+};
+
+export default Options;

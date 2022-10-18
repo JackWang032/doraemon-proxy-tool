7f60b3e9d1c20352282b3070ee23e3a730f6fdff
jialan <jialan@dtstack.com>
2022-10-18 15:13:43 +0800

    fix ip get failed when app installed

--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -1,18 +1,21 @@
 import request from '@/api/request';
-chrome.storage.local.get('config').then((res) => {
-    const config = res.config;
-    if (config.ipGetMode === 'auto') {
-        request('/api/github/get-local-ip').then((res) => {
-            if (res.success) {
-                const ip = res.data?.localIp || '';
-                chrome.storage.local.set({ ip });
-            }
-        });
-    }
-});
 
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.set({
+const getLocalIp = async () => {
+    const localData = await chrome.storage.local.get('config');
+    const { config } = localData;
+    if (config?.ipGetMode === 'auto') {
+        const res = await request('/api/github/get-local-ip');
+        if (res.success) {
+            const ip = res.data?.localIp || '';
+            chrome.storage.local.set({ ip });
+        }
+    }
+};
+
+getLocalIp()
+
+chrome.runtime.onInstalled.addListener(async () => {
+    await chrome.storage.local.set({
         proxyServers: [],
         config: {
             ipGetMode: 'auto', // ip获取方式 auto 自动获取， fixed 固定ip
@@ -20,4 +23,5 @@ chrome.runtime.onInstalled.addListener(() => {
             theme: 'light',
         },
     });
+    getLocalIp()
 });
